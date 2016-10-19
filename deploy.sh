5ebe8f83e4e9938f7f9f645de1451f5fbbf1136f
Ben Teichman <ben.l.teichman@gmail.com>
2016-10-19 11:40:13 -0400

    project subrouting and autodeploy to production with routing for existing files

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+projects=$(echo ${GB_CDN_PROJECTS} | tr ":" "\n")
+
+for proj in ${projects}; do
+  git clone "https://github.com/groupby/${proj}.git" projects/${proj}
+done
+
+${HOME}/google-cloud-sdk/bin/gcloud -q preview app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
--- redirect.py
+++ redirect.py
@@ -1,4 +1,8 @@
-class RedirectHandler(webapp.RequestHandler):
+import webapp2
+
+class RedirectHandler(webapp2.RequestHandler):
     def get(self):
         staticFile = self.request.path.split('/')[-1]
-        self.redirect('http://groupby.github.io/api-javascript/dist' + staticFile)
+        self.redirect('http://groupby.github.io/api-javascript/dist/' + staticFile)
+
+app = webapp2.WSGIApplication([('/.*', RedirectHandler)], debug=True)
