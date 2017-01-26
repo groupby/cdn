43a7b57c3616dc78674375982082818cb344ff29
Eric Hacke <ehacke@gmail.com>
2017-01-26 13:26:58 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ for proj in ${projects}; do
   git clone "https://github.com/groupby/${proj}.git" projects/${proj}
 done
 
-${HOME}/google-cloud-sdk/bin/gcloud -q preview app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
+${HOME}/google-cloud-sdk/bin/gcloud -q beta app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
