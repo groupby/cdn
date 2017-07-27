c1686b2036f6d63759ecec0bce03219484435ab6
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-27 16:58:22 -0400

    chore(test): check the output of the line count command

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,14 @@ gcloud="${HOME}/google-cloud-sdk/bin/gcloud"
 
 $gcloud -q beta app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
 all_versions=`$gcloud beta app versions list --project=$GCLOUD_PROJECT --filter service=cdn --sort-by Version | tail -n +2`
-version_count=`echo $all_versions | wc -l | awk '{print $1}'`
+version_count=`echo $all_versions | wc -l`
 to_delete=`expr $version_count - 25`
 
-echo "found existing versions"
-$gcloud beta app versions list --project=$GCLOUD_PROJECT --filter service=cdn --sort-by Version
+echo "found $version_count existing versions"
 echo "$to_delete/$version_count versions can be deleted"
 
-if [ "$to_delete" -gt 0 ]; then
-  echo "removing old versions of CDN"
-  versions=`echo $all_versions | head -n $to_delete | awk '{ print $2; }' | tr '\n' ' '`
-  $gcloud beta app delete $versions --project=$GCLOUD_PROJECT --service=cdn
-fi
+# if [ "$to_delete" -gt 0 ]; then
+#   echo "removing old versions of CDN"
+#   versions=`echo $all_versions | head -n $to_delete | awk '{ print $2; }' | tr '\n' ' '`
+#   $gcloud beta app delete $versions --project=$GCLOUD_PROJECT --service=cdn
+# fi
