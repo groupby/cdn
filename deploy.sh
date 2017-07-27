c9db15b53e7319dca222da65bfe872fae0553abb
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-27 16:34:03 -0400

    add comment and pre-filter by project

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,12 @@ done
 gcloud="${HOME}/google-cloud-sdk/bin/gcloud"
 
 $gcloud -q beta app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
-all_versions=`$gcloud -q beta app versions list --filter=service=cdn --sort-by=Version | tail -n +2`
+all_versions=`$gcloud -q beta app versions list --project=$GCLOUD_PROJECT --filter=service=cdn --sort-by=Version | tail -n +2`
 version_count=`echo $all_versions | wc -l`
 to_delete=`echo "$version_count - 25" | bc`
 
 if [ "$to_delete" -gt 0 ]; then
+  echo removing old versions of CDN
   versions=`echo $all_versions | head -n $to_delete | awk '{ print $2; }' | tr '\n' ' '`
-  $gcloud -q beta app delete $versions --service=cdn
+  $gcloud -q beta app delete $versions --project=$GCLOUD_PROJECT --service=cdn
 fi
