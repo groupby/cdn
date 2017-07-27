66fea28c38be46f2150b8c9ac612c40eddce8796
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-27 17:38:00 -0400

    fix(deploy): switch to non-interactive command

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ done
 gcloud="${HOME}/google-cloud-sdk/bin/gcloud"
 
 $gcloud -q beta app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
-get_versions="$gcloud beta app versions list --project=$GCLOUD_PROJECT --filter service=cdn --sort-by Version"
+get_versions="$gcloud -q beta app versions list --project=$GCLOUD_PROJECT --filter service=cdn --sort-by Version"
 version_count=`$get_versions | tail -n +2 | wc -l | awk '{ print $1; }'`
 to_delete=`expr $version_count - 25`
 
@@ -16,5 +16,5 @@ echo "$to_delete/$version_count versions can be deleted"
 if [ "$to_delete" -gt 0 ]; then
   echo "removing old versions of CDN"
   versions=`$get_versions | tail -n +2 | head -n $to_delete | awk '{ print $2; }' | tr '\n' ' '`
-  $gcloud beta app versions delete $versions --project=$GCLOUD_PROJECT --service=cdn
+  $gcloud -q beta app versions delete $versions --project=$GCLOUD_PROJECT --service=cdn
 fi
