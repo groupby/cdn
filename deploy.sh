f18bf11664a0a47e764e555968a818b6c4b794bd
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-27 16:31:43 -0400

    only request version list once

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,11 @@ done
 gcloud="${HOME}/google-cloud-sdk/bin/gcloud"
 
 $gcloud -q beta app deploy app.yml --promote --project=$GCLOUD_PROJECT --verbosity=info
-total_versions=`$gcloud -q beta app versions list --filter=service=cdn --sort-by=Version | tail -n +2 | wc -l`
-to_delete=`echo "$total_versions - 25" | bc`
+all_versions=`$gcloud -q beta app versions list --filter=service=cdn --sort-by=Version | tail -n +2`
+version_count=`echo $all_versions | wc -l`
+to_delete=`echo "$version_count - 25" | bc`
 
 if [ "$to_delete" -gt 0 ]; then
-  versions=`$gcloud -q beta app versions list --filter=service=cdn --sort-by=Version --limit=$to_delete | tail -n +2 | awk '{ print $2; }' | tr '\n' ' '`
+  versions=`echo $all_versions | head -n $to_delete | awk '{ print $2; }' | tr '\n' ' '`
   $gcloud -q beta app delete $versions --service=cdn
 fi
