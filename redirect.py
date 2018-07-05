9e6834705b6eae33002b29d89754a833bfcfaed6
Osman Currim <osman.currim@groupbyinc.com>
2018-07-05 15:45:53 -0400

    update for gcs backed cdn

--- redirect.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-
-class RedirectHandler(webapp2.RequestHandler):
-    def get(self):
-        staticFile = self.request.path.split('/')[-1]
-        self.redirect('http://groupby.github.io/api-javascript/dist/' + staticFile)
-
-app = webapp2.WSGIApplication([('/.*', RedirectHandler)], debug=True)
